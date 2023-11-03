5be4d1a18c2c83d537cfe0a9e28e85e7c42ac0f3
dhirajtek13 <dhirajtek13@gmail.com>
2023-11-03 22:29:47 +0530

    Update response_data.js

--- response_data.js
+++ response_data.js
@@ -1,21 +1,21 @@
 var response_data = {
     "requested_token": {
-      "C1-PRY-01": {
-        token: "C1-PRY-01",
-        time: 1694596190966
-      },
-      "C1-PRY-33": {
-        token: "C1-PRY-33",
-        time: 1694596339163
-      },
-      "C1-PRY-04": {
-        token: "C1-PRY-04",
-        time: 1694596328163
-      },
+    //   "C1-PRY-01": {
+    //     token: "C1-PRY-01",
+    //     time: 1694596190966
+    //   },
+    //   "C1-PRY-33": {
+    //     token: "C1-PRY-33",
+    //     time: 1694596339163
+    //   },
+    //   "C1-PRY-04": {
+    //     token: "C1-PRY-04",
+    //     time: 1694596328163
+    //   },
     },
     "returned_token": [
-      "C1-PRY-24",
-      "C1-PRY-02",
+    //   "C1-PRY-24",
+    //   "C1-PRY-02",
     ]
   };
   export default response_data;
